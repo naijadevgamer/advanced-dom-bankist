83b05a4cb84b588c52ce869faf40ac644cdeaa12
thenaijagamer <sabdullahialaba50@gmail.com>
2024-01-31 15:28:03 +0100

    Make some minor refactoring

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  // section.classList.add('section--hidden');
+  section.classList.add('section--hidden');
 });
 
 //////////////////////////////////////////////////
